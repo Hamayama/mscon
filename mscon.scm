ad3cd1436ae10b53b08bfbf262655159315e8131
Hamayama <hamay1010@gmail.com>
2014-06-22 17:43:04 +0900

    first commit

---
+++ mscon.scm
@@ -0,0 +1,272 @@
+;; -*- coding: utf-8 -*-
+;;
+;; mscon.scm
+;; 2014-6-22 v1.00
+;;
+;; ＜内容＞
+;;   Windows のコマンドプロンプトで Gauche(gosh.exe) を使うときに、
+;;   コンソールの制御を可能とするモジュールです。
+;;
+;; ＜インストール方法＞
+;;   mscon.scm を Gauche でロード可能なフォルダにコピーします。
+;;   (例えば (gauche-site-library-directory) で表示されるフォルダ等)
+;;
+;; ＜使い方＞
+;;   (use mscon)                  ; モジュールをロードします
+;;   (cls)                        ; 画面をクリアします(コマンドのため遅い)
+;;   (cls2)                       ; 画面をクリアします2(*)
+;;   (screen-width)               ; 画面の幅を取得します(文字数)
+;;   (screen-height)              ; 画面の高さを取得します(文字数)
+;;   (cursor-on)                  ; カーソルを表示します
+;;   (cursor-off)                 ; カーソルを非表示にします
+;;   (color COL_GREEN)            ; 色を設定します
+;;   (locate 10 10)               ; カーソルを座標(x,y)に移動します
+;;   (print "HIT ANY KEY!")       ;
+;;   (keywait)                    ; キーボードの入力を待ちます
+;;   (keystate)                   ; キーボードの状態を取得します(*)
+;;   (keystate-test)              ; キーボード状態取得テスト用です(*)
+;;   (keywait2)                   ; キーボードの入力を待ちます2(タイムアウト設定可)(*)
+;;   (keyclear)                   ; キーボードの入力をクリアします(*)
+;;   (puttext "ABCDE" 10 10)      ; 座標(x,y)に文字列を表示します
+;;   (putcolor 5 10 10 COL_GREEN) ; 座標(x,y)からn文字分に色を設定します(*)
+;;
+;;   (*)マークがある命令は Gauche v0.9.3.3 では使用できません。
+;;      使用可能かどうかは (mscon-all-available?) でチェックできます。
+;;
+;; ＜注意事項＞
+;;   (1)writeやdisplayが、指定したカーソル位置に表示しない(原因不明)。
+;;
+;;   (2)最下行にprintで表示すると、改行のためスクロールしてしまう。
+;;
+;;   (3)puttextが、colorで指定した色では表示しない。
+;;      (色をつけるにはputcolorと併用する必要がある)
+;;
+(define-module mscon
+  (use gauche.uvector)
+  (use os.windows)
+  (export
+    mscon-all-available?
+    cls cls2 screen-width screen-height cursor-on cursor-off
+    color locate keywait keystate keystate-test keywait2 keyclear
+    puttext putcolor
+    COL_BLACK       COL_DARK_BLUE   COL_DARK_GREEN  COL_DARK_CYAN
+    COL_DARK_RED    COL_DARK_VIOLET COL_DARK_YELLOW COL_GRAY
+    COL_DARK_GRAY   COL_BLUE        COL_GREEN       COL_CYAN
+    COL_RED         COL_VIOLET      COL_YELLOW      COL_WHITE
+    COL_BLUE_MASK   COL_GREEN_MASK  COL_RED_MASK    COL_INTENSITY
+    VK_SHIFT        VK_CONTROL      VK_MENU))
+(select-module mscon)
+
+
+;; 標準入出力のハンドルの保持
+;; (保持しておかないとエラーになる。Gaucheの開発最新版では修正ずみ)
+(define stdin-handle  (sys-get-std-handle STD_INPUT_HANDLE))
+(define stdout-handle (sys-get-std-handle STD_OUTPUT_HANDLE))
+(define stderr-handle (sys-get-std-handle STD_ERROR_HANDLE))
+
+
+;; 色の定数
+(define-values
+  (COL_BLACK       COL_DARK_BLUE   COL_DARK_GREEN  COL_DARK_CYAN
+   COL_DARK_RED    COL_DARK_VIOLET COL_DARK_YELLOW COL_GRAY
+   COL_DARK_GRAY   COL_BLUE        COL_GREEN       COL_CYAN
+   COL_RED         COL_VIOLET      COL_YELLOW      COL_WHITE)
+  (apply values (iota 16)))
+(define COL_BLUE_MASK  #x1)
+(define COL_GREEN_MASK #x2)
+(define COL_RED_MASK   #x4)
+(define COL_INTENSITY  #x8)
+
+;; イベントの定数
+(define KEY_EVENT 1)
+
+;; コントロールキーの定数
+(define RIGHT_ALT_PRESSED  #x01)
+(define LEFT_ALT_PRESSED   #x02)
+(define RIGHT_CTRL_PRESSED #x04)
+(define LEFT_CTRL_PRESSED  #x08)
+(define SHIFT_PRESSED      #x10)
+
+;; 仮想キーコードの定数
+(define VK_SHIFT   16)
+(define VK_CONTROL 17)
+(define VK_MENU    18)
+
+
+;; 全機能利用可能か
+(define (mscon-all-available?)
+  (guard (exc
+          ((<error> exc) #f))
+         (procedure? sys-fill-console-output-character)
+         (procedure? sys-fill-console-output-attribute)
+         (procedure? sys-flush-console-input-buffer)
+         #t))
+
+;; 画面クリア
+(define (cls)
+  (sys-system "cls")
+  (undefined))
+
+;; 画面クリア2
+(define (cls2 :optional (fc COL_GRAY) (bc COL_BLACK))
+  (let1 hdl (sys-get-std-handle STD_OUTPUT_HANDLE)
+    (let1 cinfo (sys-get-console-screen-buffer-info hdl)
+      (let ((bw (slot-ref cinfo 'size.x))
+            (bh (slot-ref cinfo 'size.y))
+            (cattr (get-color-attr fc bc)))
+        (sys-fill-console-output-attribute hdl cattr (* bw bh) 0 0)
+        (sys-fill-console-output-character hdl " "   (* bw bh) 0 0)
+        (sys-set-console-cursor-position hdl 0 0)))))
+
+;; 画面の幅を取得
+(define (screen-width)
+  (let1 hdl (sys-get-std-handle STD_OUTPUT_HANDLE)
+    (let1 cinfo (sys-get-console-screen-buffer-info hdl)
+      (let ((wl (slot-ref cinfo 'window.left))
+            (wr (slot-ref cinfo 'window.right)))
+        (+ (- wr wl) 1)))))
+
+;; 画面の高さを取得
+(define (screen-height)
+  (let1 hdl (sys-get-std-handle STD_OUTPUT_HANDLE)
+    (let1 cinfo (sys-get-console-screen-buffer-info hdl)
+      (let ((wt (slot-ref cinfo 'window.top))
+            (wb (slot-ref cinfo 'window.bottom)))
+        (+ (- wb wt) 1)))))
+
+;; カーソル表示
+(define (cursor-on)
+  (let1 hdl (sys-get-std-handle STD_OUTPUT_HANDLE)
+    (receive (sz v) (sys-get-console-cursor-info hdl)
+      (sys-set-console-cursor-info hdl sz #t))))
+
+;; カーソル非表示
+(define (cursor-off)
+  (let1 hdl (sys-get-std-handle STD_OUTPUT_HANDLE)
+    (receive (sz v) (sys-get-console-cursor-info hdl)
+      (sys-set-console-cursor-info hdl sz #f))))
+
+;; 色属性の取得(内部処理用)
+(define (get-color-attr fc bc)
+  (let1 cattr 0
+    (if (logtest fc COL_BLUE_MASK)  (set! cattr (logior cattr FOREGROUND_BLUE)))
+    (if (logtest fc COL_GREEN_MASK) (set! cattr (logior cattr FOREGROUND_GREEN)))
+    (if (logtest fc COL_RED_MASK)   (set! cattr (logior cattr FOREGROUND_RED)))
+    (if (logtest fc COL_INTENSITY)  (set! cattr (logior cattr FOREGROUND_INTENSITY)))
+    (if (logtest bc COL_BLUE_MASK)  (set! cattr (logior cattr BACKGROUND_BLUE)))
+    (if (logtest bc COL_GREEN_MASK) (set! cattr (logior cattr BACKGROUND_GREEN)))
+    (if (logtest bc COL_RED_MASK)   (set! cattr (logior cattr BACKGROUND_RED)))
+    (if (logtest bc COL_INTENSITY)  (set! cattr (logior cattr BACKGROUND_INTENSITY)))
+    cattr))
+
+;; 色設定
+(define (color :optional (fc COL_GRAY) (bc COL_BLACK))
+  (let ((hdl (sys-get-std-handle STD_OUTPUT_HANDLE))
+        (cattr (get-color-attr fc bc)))
+    (sys-set-console-text-attribute hdl cattr)))
+
+;; カーソル移動
+(define (locate x y)
+  (let1 hdl (sys-get-std-handle STD_OUTPUT_HANDLE)
+    (sys-set-console-cursor-position hdl x y)))
+
+;; キーボード入力待ち
+(define (keywait)
+  (let ((hdl     (sys-get-std-handle STD_INPUT_HANDLE))
+        (cmode   0))
+    (set! cmode (sys-get-console-mode hdl))
+    (sys-set-console-mode hdl 0)
+    (let1 ch (read-char)
+      (sys-set-console-mode hdl cmode)
+      ch)))
+
+;; キーボード状態取得
+(define (keystate)
+  (let ((hdl     (sys-get-std-handle STD_INPUT_HANDLE))
+        (cmode   0)
+        (done    #f)
+        (retlist '()))
+    (set! cmode (sys-get-console-mode hdl))
+    (sys-set-console-mode hdl 0)
+    (while (not done)
+      (let1 irlist (sys-peek-console-input hdl)
+        (if (null? irlist)
+          (set! done #t)
+          (let ((ir  (car irlist))
+                (evt 0))
+            (set! evt (slot-ref ir 'event-type))
+            (if (= evt KEY_EVENT)
+              (let ((kdown (if (slot-ref ir 'key.down) 1 0))
+                    (ch    (slot-ref ir 'key.unicode-char))
+                    (vk    (slot-ref ir 'key.virtual-key-code))
+                    (ctls  (slot-ref ir 'key.control-key-state))
+                    (sft   0)
+                    (ctl   0)
+                    (alt   0))
+                (if (logtest ctls SHIFT_PRESSED) (set! sft 1))
+                (if (logtest ctls (logior RIGHT_CTRL_PRESSED LEFT_CTRL_PRESSED)) (set! ctl 1))
+                (if (logtest ctls (logior RIGHT_ALT_PRESSED  LEFT_ALT_PRESSED )) (set! alt 1))
+                ;(set! retlist (append! retlist (list (list kdown ch vk sft ctl alt))))
+                ;(set! retlist (cons (list kdown ch vk sft ctl alt) retlist)) ; 最後にリバースする必要あり
+                (push! retlist (list kdown ch vk sft ctl alt)) ; 最後にリバースする必要あり
+                ))
+            (sys-read-console-input hdl)))))
+    (sys-set-console-mode hdl cmode)
+    (reverse retlist)))
+
+;; キーボード状態取得のテスト
+(define (keystate-test)
+  (print "HIT ANY KEY! ([ESC] TO EXIT)")
+  (let1 done #f
+    (while (not done)
+      (let1 kslist (keystate)
+        ;(print kslist)
+        (while (not (null? kslist))
+          (receive (kdown ch vk sft ctl alt) (apply values (car kslist))
+            (cond
+              ((and (= kdown 1) (= vk 27)) (set! done #t))
+              ((not (and (= kdown 0) (= ch 0) (= vk 0) (= sft 0) (= ctl 0) (= alt 0)))
+                (print " keydown=" kdown " unicode-char=" ch " virtual-key-code=" vk " shift=" sft " ctrl=" ctl " alt=" alt))))
+          (set! kslist (cdr kslist)))
+        (sys-nanosleep (* 100 1000000))))) ; 100msec
+  (undefined))
+
+;; キーボード入力待ち2
+(define (keywait2 :optional (timeout 0) (interval 100))
+  (let ((done      #f)
+        (ks        '())
+        (timecount 0))
+    (while (not done)
+      (let1 kslist (keystate)
+        ;(print kslist)
+        (while (not (null? kslist))
+          (set! ks (car kslist))
+          (receive (kdown ch vk sft ctl alt) (apply values ks)
+            ;; [shift]と[ctrl]と[alt]は除外
+            (if (and (= kdown 1) (not (or (= vk VK_SHIFT) (= vk VK_CONTROL) (= vk VK_MENU) (and (>= vk #xA0) (<= vk #xA5)))))
+              (set! done #t)))
+          (set! kslist (cdr kslist)))
+        (sys-nanosleep (* interval 1000000))
+        (if (> timeout 0)
+          (begin
+            (set! timecount (+ timecount interval))
+            (if (>= timecount timeout)
+              (set! done #t))))))
+    ks))
+
+;; キーボード入力クリア
+(define (keyclear)
+  (let1 hdl (sys-get-std-handle STD_INPUT_HANDLE)
+    (sys-flush-console-input-buffer hdl)))
+
+;; 文字列表示
+(define (puttext str x y)
+  (let1 hdl (sys-get-std-handle STD_OUTPUT_HANDLE)
+    (sys-write-console-output-character hdl str x y)))
+
+;; 文字数と座標を指定して色設定
+(define (putcolor n x y :optional (fc COL_GRAY) (bc COL_BLACK))
+  (let ((hdl (sys-get-std-handle STD_OUTPUT_HANDLE))
+        (cattr (get-color-attr fc bc)))
+    (sys-fill-console-output-attribute hdl cattr n x y)))
+
---
+++ test1001.scm
@@ -0,0 +1,143 @@
+;; -*- coding: utf-8 -*-
+;;
+;; Windowsコンソールで花火の表示
+;;
+(add-load-path "." :relative)
+(use mscon)
+(use math.const)
+(use math.mt-random)
+
+;; 乱数
+(define m (make <mersenne-twister> :seed (sys-time)))
+(define (randint n1 n2)
+  (+ (mt-random-integer m (+ (- n2 n1) 1)) n1))
+
+;; 花火情報
+;;   act   行動モード(=0:なし,=1:上昇中,=2:爆発後)
+;;   tim   生存カウンタ(0になったら消える)
+;;   x     X座標
+;;   y     Y座標
+;;   rad   角度
+;;   spd   速度
+;;   divx  X方向の速度の補正係数(例えば2にするとX方向の速度が半分になる)
+;;   divy  Y方向の速度の補正係数(例えば2にするとY方向の速度が半分になる)
+;;   col   色
+(define-class <hanabi> () (act tim x y rad spd divx divy col))
+
+;; 各種初期化
+(define done     #f)              ; ループフラグ
+(define wait     100)             ; ウェイト時間(msec)
+(define wd       (screen-width))  ; 画面の幅(文字数)
+(define ht       (screen-height)) ; 画面の高さ(文字数)
+(define hanalist '())             ; 花火のリスト
+(define sc       0)               ; カウンタ(タイミング調整用)
+
+;; メインループ
+(if (mscon-all-available?) (keyclear))
+(while (not done)
+  (if (mscon-all-available?) (cls2) (cls))
+  (color COL_YELLOW)
+  (locate 0 0)
+  (if (mscon-all-available?)
+    (print "HIT ANY KEY TO EXIT")
+    (print "HIT CTRL-C CTRL-C KEY TO EXIT"))
+  (color COL_WHITE)
+
+  ;; 花火の生成
+  (inc! sc)
+  ;(print sc)
+  (if (> sc 1000000) (set! sc 1))
+  (if (and (= (modulo sc 2) 0) (<= (randint 1 6) 3))
+    (let1 hb (make <hanabi>)
+      (slot-set! hb 'act  1)
+      (slot-set! hb 'tim  (randint 4 8))
+      (slot-set! hb 'x    (randint 5 (- wd 5 1)))
+      (slot-set! hb 'y    (- ht 1))
+      (slot-set! hb 'rad  (- pi/2))
+      (slot-set! hb 'spd  5)
+      (slot-set! hb 'divx 1)
+      (slot-set! hb 'divy 2)
+      (slot-set! hb 'col  15)
+      (push! hanalist hb)))
+
+  ;; 花火の移動と爆発
+  (for-each
+    (lambda (hb)
+      (let ((act  (slot-ref hb 'act))
+            (tim  (slot-ref hb 'tim))
+            (x    (slot-ref hb 'x))
+            (y    (slot-ref hb 'y))
+            (rad  (slot-ref hb 'rad))
+            (spd  (slot-ref hb 'spd))
+            (divx (slot-ref hb 'divx))
+            (divy (slot-ref hb 'divy))
+            (col  (slot-ref hb 'col))
+            (vx   0)
+            (vy   0))
+        (if (> act 0)
+          (begin
+            (set! vx  (/. (*. spd (cos rad)) divx))
+            (set! vy  (/. (*. spd (sin rad)) divy))
+            (set! x   (+ x vx))
+            (set! y   (+ y vy))
+            (set! spd (- spd 0.2))
+            (dec! tim)
+            (slot-set! hb 'x   x)
+            (slot-set! hb 'y   y)
+            (slot-set! hb 'spd spd)
+            (slot-set! hb 'tim tim)
+            (if (<= tim 0)
+              (begin
+                (if (= act 1)
+                  (do ((i     0 (+ i 1))
+                       (rad1  0 (+ rad1 pi/4))
+                       (cycle 0))
+                      ((>= i 16) #f)
+                    (let1 hb2 (make <hanabi>)
+                      (if (= (modulo i 8) 0) (inc! cycle))
+                      (slot-set! hb2 'act  2)
+                      (slot-set! hb2 'tim  5)
+                      (slot-set! hb2 'x    x)
+                      (slot-set! hb2 'y    y)
+                      (slot-set! hb2 'rad  rad1)
+                      (slot-set! hb2 'spd  (* cycle 2))
+                      (slot-set! hb2 'divx 1)
+                      (slot-set! hb2 'divy 2)
+                      (slot-set! hb2 'col  col)
+                      (push! hanalist hb2))))
+                (slot-set! hb 'act 0)))
+            (if (or (< x 0) (>= x wd) (< y 0) (>= y ht))
+              (slot-set! hb 'act 0))))))
+    hanalist)
+
+  ;; 花火の表示
+  (for-each
+    (lambda (hb)
+      (let ((act  (slot-ref hb 'act))
+            (x    (slot-ref hb 'x))
+            (y    (slot-ref hb 'y))
+            (col  (slot-ref hb 'col)))
+        (if (> act 0)
+          (begin
+            (set! x  (x->integer x))
+            (set! y  (x->integer y))
+            (if (mscon-all-available?) (putcolor 1 x y col))
+            (puttext "*" x y)))))
+    hanalist)
+
+  ;; 花火の消去
+  (set! hanalist
+    (remove!
+      (lambda (hb)
+        (let1 act (slot-ref hb 'act)
+          (if (<= act 0) #t #f)))
+      hanalist))
+  ;(print hanalist)
+
+  ;; キー入力のチェックとウェイト
+  (if (mscon-all-available?)
+    (let1 ks (keywait2 wait wait)
+      (if (not (null? ks)) (set! done #t)))
+    (sys-nanosleep (* (/. wait 2) 1000000)))
+  )
+
